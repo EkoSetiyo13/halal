ae2e3af864c363907c6201391537f6414c240b07
nadhifikbarw <nadhifikbarw@gmail.com>
2021-07-31 16:00:40 +0700

    ADD: refactor class

--- app/Http/Controllers/UmkmController.php
+++ app/Http/Controllers/UmkmController.php
@@ -2,79 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use Auth;
+use File;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Maatwebsite\Excel\Facades\Excel;
+use Illuminate\Support\Facades\Storage;
+
+use App\Umkm;
+use App\JenisTernak;
 use App\Imports\UmkmsImport;
 use App\Exports\UmkmsExport;
-use App\Umkm;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Storage;
-use Maatwebsite\Excel\Facades\Excel;
 
-use Illuminate\Support\Str;
-use App\FormPenyuplaiQurban;
-use App\JenisTernak;
-use File;
-use Auth;
 
 class UmkmController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
         $umkms = Umkm::orderBy('id', 'ASC')->get();
         return view('umkm.index', compact('umkms'));
     }
 
-    public function kader()
-    {
-        $umkms = Umkm::Where('nama_umkm', '=', '-')->orderBy('id', 'ASC')->get();
-        //return response()->json($umkms);
-        return view('umkm.kader', compact('umkms'));
-    }
-
-    public function umkm()
-    {
-        $umkms = Umkm::Where('status', '=', true)->Where('nama_umkm', '!=', '-')->orderBy('id', 'ASC')->get();
-        return view('umkm.umkm', compact('umkms'));
-    }
-
-
-
-
-    public function import(Request $request)
-    {
-        $this->validate($request, [
-            'file' => 'required|mimes:csv,xls,xlsx'
-        ]);
-
-        $file = $request->file('file');
-        $nama_file = $file->hashName();
-        $path = $file->storeAs('public/excel/',$nama_file);
-        $import = Excel::import(new UmkmsImport(), storage_path('app/public/excel/'.$nama_file));
-        Storage::delete($path);
-
-        //return response()->json($import);
-
-        if($import) {
-            //redirect
-            return redirect()->route('users.index')->with(['success' => 'Data Berhasil Diimport!']);
-        } else {
-            //redirect
-            return redirect()->route('users.index')->with(['error' => 'Data Gagal Diimport!']);
-        }
-    }
-
-    public function export()
-    {
-        return Excel::download(new UmkmsExport(), 'users.xlsx');
-    }
-
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
     public function create()
     {
-        
         return view('umkm.create');
     }
 
@@ -153,6 +108,44 @@ class UmkmController extends Controller
         return redirect(route('umkm.index'))->with(['success' => 'Produk Sudah Dihapus']);
     }
 
-   
+    public function kader()
+    {
+        $umkms = Umkm::Where('nama_umkm', '=', '-')->orderBy('id', 'ASC')->get();
+        //return response()->json($umkms);
+        return view('umkm.kader', compact('umkms'));
+    }
 
+    public function umkm()
+    {
+        $umkms = Umkm::Where('status', '=', true)->Where('nama_umkm', '!=', '-')->orderBy('id', 'ASC')->get();
+        return view('umkm.umkm', compact('umkms'));
+    }
+
+    public function import(Request $request)
+    {
+        $this->validate($request, [
+            'file' => 'required|mimes:csv,xls,xlsx'
+        ]);
+
+        $file = $request->file('file');
+        $nama_file = $file->hashName();
+        $path = $file->storeAs('public/excel/',$nama_file);
+        $import = Excel::import(new UmkmsImport(), storage_path('app/public/excel/'.$nama_file));
+        Storage::delete($path);
+
+        //return response()->json($import);
+
+        if($import) {
+            //redirect
+            return redirect()->route('users.index')->with(['success' => 'Data Berhasil Diimport!']);
+        } else {
+            //redirect
+            return redirect()->route('users.index')->with(['error' => 'Data Gagal Diimport!']);
+        }
+    }
+
+    public function export()
+    {
+        return Excel::download(new UmkmsExport(), 'users.xlsx');
+    }
 }
