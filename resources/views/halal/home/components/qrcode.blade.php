374e6775eca31d7042e71f881d501e6c967d05d6
nadhifikbarw <nadhifikbarw@gmail.com>
2021-08-15 17:33:09 +0700

    ADD: Change QRCode handling

---
+++ resources/views/halal/home/components/qrcode.blade.php
@@ -0,0 +1,72 @@
+<div class="container">
+  <div class="row s_product_inner">
+    <div class="col-lg-12">
+      <section class="product_description_area"
+               style="margin-top: -10px; margin-bottom: 100px">
+        <div class="container">
+          <ul class="nav nav-tabs" id="myTab" role="tablist">
+            <li class="nav-item">
+              <a class="nav-link active show" id="qrcode-tab" data-toggle="tab" href="#qrcode"
+                 role="tab"
+                 aria-controls="home" aria-selected="true">QR Code</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" id="qrcode2-tab" data-toggle="tab" href="#qrcode2"
+                 role="tab"
+                 aria-controls="profile" aria-selected="false">QR Code dengan Logo</a>
+            </li>
+          </ul>
+          <div class="tab-content" id="myTabContent">
+            <div class="tab-pane fade show active" id="qrcode" role="tabpanel"
+                 aria-labelledby="qrcode-tab"
+                 style="color: black">
+              <div class="table-responsive">
+                <table class="table">
+                  <tbody>
+                  <tr>
+                    <td class="text-center">
+                      <h5 style="color: black">Nomer Binaan : {{ $idBinaan }}</h5>
+                      <a href="{{ $plainQRCode }}" download>
+                        <img width="300px" src="{{ $plainQRCode }}">
+                      </a>
+                      <p>*Pasang kode ini di web anda untuk menampilkan QR Code anda di web</p>
+                      <p><b>*Berlaku hingga Agustus 2022</b></p>
+                      <textarea readonly rows="15" id="myQrCode" name="description"
+                                placeholder="informasi mengenai deskripsi yang menarik"
+                                id="description"
+                                class="form-control"><a href="{{ $urlBinaan }}"><img src="{{ $plainQRCode }}"></a></textarea>
+                    </td>
+                  </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+            <div class="tab-pane fade" id="qrcode2" role="tabpanel"
+                 aria-labelledby="qrcode-tab">
+              <div class="table-responsive">
+                <table class="table">
+                  <tbody>
+                  <tr>
+                    <td class="text-center">
+                      <h5 style="color: black">Nomer Binaan : {{ $idBinaan }}</h5>
+                      <a href="{{ $logoQRCode }}" download>
+                        <img width="300px" src="{{ $logoQRCode }}">
+                      </a>
+                      <p>*Pasang kode ini di web anda untuk menampilkan QR Code anda di web</p>
+                      <p><b>*Berlaku hingga Agustus 2022</b></p>
+                      <textarea readonly rows="15" id="myQrCodeLogo" name="description"
+                                placeholder="informasi mengenai deskripsi yang menarik"
+                                id="description"
+                                class="form-control"><a href="{{ $urlBinaan }}"><img src="{{ $logoQRCode }}"></a></textarea>
+                    </td>
+                  </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  </div>
+</div>
--- resources/views/halal/home/detail-binaan.blade.php
+++ resources/views/halal/home/detail-binaan.blade.php
@@ -5,22 +5,17 @@
 @endsection
 
 @section('content')
-
-  {{-- @php
-    $values = explode(",", $binaan->pengiriman);
-  @endphp --}}
   <!--================Home Banner Area =================-->
-  <section class="banner_halal" style="background-image: ">
+  <section class="banner_halal">
     <div class="banner_inner d-flex align-items-center">
       <div class="container">
         <div class="banner_content text-center">
-          <h2>Informasi Binaan</h2>
+          <h2>Informasi Binaan <br> {{ $binaan->nama_umkm }}</h2>
         </div>
       </div>
     </div>
   </section>
   <!--================End Home Banner Area =================-->
-
   <div class="product_image_area">
     <div class="container">
       <div class="row s_product_inner">
@@ -40,28 +35,10 @@
                          src="{{ asset('storage/products/' . $binaan->image) }}" alt="{{ $binaan->nama_umkm }}">
                   </div>
                 </div>
-
               @endif
-              {{-- <div class="carousel-inner">
-                <div class="carousel-item active">
-                  <img class="d-block w-100" style="height: 300px" src="{{ asset('storage/products/' . $binaan->image) }}" alt="{{ $binaan->nama_umkm }}">
-                </div>
-              </div> --}}
-              {{-- <div class="carousel-inner text-center">
-                <div class="carousel-item active">
-                  <a href="data:image/png;base64, {!! base64_encode($downloadQrCode) !!} " download>
-                    <img src="data:image/png;base64, {!! base64_encode($qrcode) !!} ">
-                  </a>
-                  <a href="data:image/png;base64, {!! base64_encode($downloadQrcodeWithLogo) !!} " download>
-                    <img src="data:image/png;base64, {!! base64_encode($qrcodeWithLogo) !!} ">
-                  </a>
-                </div>
-              </div> --}}
             </div>
           </div>
         </div>
-
-
         <div class="col-lg-8">
           <section class="product_description_area" style="margin-top: -10px; margin-bottom: 100px">
             <div class="container">
@@ -86,7 +63,7 @@
                           <h5><b style="color: black">No Binaan</b></h5>
                         </td>
                         <td>
-                          <h5> : {!! $binaan->no_umkm !!}</h5>
+                          <h5> : {{ $binaan->no_umkm }}</h5>
                         </td>
                       </tr>
                       <tr>
@@ -94,7 +71,7 @@
                           <h5><b style="color: black">Nama Lengkap</b></h5>
                         </td>
                         <td>
-                          <h5>: {!! $binaan->nama_pemilik !!}</h5>
+                          <h5>: {{ $binaan->nama_pemilik }}</h5>
                         </td>
                       </tr>
                       <tr>
@@ -102,7 +79,7 @@
                           <h5><b style="color: black">Nama UMKM</b></h5>
                         </td>
                         <td>
-                          <h5>: {!! $binaan->nama_umkm !!}</h5>
+                          <h5>: {{ $binaan->nama_umkm }}</h5>
                         </td>
                       </tr>
                       <tr>
@@ -110,7 +87,7 @@
                           <h5><b style="color: black">Nama Produk</b></h5>
                         </td>
                         <td>
-                          <h5>: {!! $binaan->nama_produk !!}</h5>
+                          <h5>: {{ $binaan->nama_produk }}</h5>
                         </td>
                       </tr>
                       <tr>
@@ -118,9 +95,44 @@
                           <h5><b style="color: black">Tipe Binaan</b></h5>
                         </td>
                         <td>
-                          <h5>: {!! $binaan->tipe_binaan !!}</h5>
+                          <h5>: {{ $binaan->tipe_binaan }}</h5>
                         </td>
                       </tr>
+                      <tr>
+                        <td>
+                          <h5><b style="color: black">No BPOM</b></h5>
+                        </td>
+                        <td>
+                          <h5>: {{ $binaan->bpom }}</h5>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <h5><b style="color: black">No PIRT</b></h5>
+                        </td>
+                        <td>
+                          <h5>: {{ $binaan->pirt }}</h5>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <h5><b style="color: black">No Sertifikat Halal</b></h5>
+                        </td>
+                        <td>
+                          <h5>: {{ $binaan->sertifikat_halal }}</h5>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <h5><b style="color: black">Video Produk</b></h5>
+                        </td>
+                        <td>
+                          @if($binaan->video ?? false)
+                            <h5>: <a href="{{ $binaan->video }}">Kunjungi</a></h5>
+                          @endif
+                        </td>
+                      </tr>
+
                       </tbody>
                     </table>
                   </div>
@@ -134,7 +146,7 @@
                           <h5>Alamat</h5>
                         </td>
                         <td>
-                          <h5>{{ $binaan->alamat }} , {{ $binaan->desa }}. {{ $binaan->kecamatan }}
+                          <h5>: {{ $binaan->alamat }} , {{ $binaan->desa }}. {{ $binaan->kecamatan }}
                             , {{ $binaan->kota }} </h5>
                         </td>
                       </tr>
@@ -143,7 +155,7 @@
                           <h5>No Whatsapp</h5>
                         </td>
                         <td>
-                          <h5>{{ $binaan->no_wa }}</h5>
+                          <h5>: {{ $binaan->no_wa }}</h5>
                         </td>
                       </tr>
                       <tr>
@@ -151,7 +163,7 @@
                           <h5>Email</h5>
                         </td>
                         <td>
-                          <h5>{{ $binaan->email }}</h5>
+                          <h5>: {{ $binaan->email }}</h5>
                         </td>
                       </tr>
                       <tr>
@@ -159,7 +171,7 @@
                           <h5>Instagram</h5>
                         </td>
                         <td>
-                          <h5>{{ $binaan->instagram }}</h5>
+                          <h5>: {{ $binaan->instagram }}</h5>
                         </td>
                       </tr>
                       <tr>
@@ -167,7 +179,21 @@
                           <h5>Facebook</h5>
                         </td>
                         <td>
-                          <h5>{{ $binaan->facebook }}</h5>
+                          <h5>: {{ $binaan->facebook }}</h5>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <h5>Website/Toko Onine</h5>
+                        </td>
+                        <td>
+                          <h5>:
+                            @foreach(['bukalapak', 'shopee', 'tokopedia', 'website'] as $field)
+                              @if($binaan->{$field} ?? false)
+                                <a href="{{ $binaan->{$field} }}">{{ ucfirst($field) }}</a>
+                              @endif
+                            @endforeach
+                          </h5>
                         </td>
                       </tr>
                       </tbody>
@@ -179,98 +205,22 @@
           </section>
         </div>
       </div>
-
-
     </div>
   </div>
-
-  {{-- untuk code qrcode --}}
-
-  <div class="container">
-    <div class="row s_product_inner">
-      <div class="col-lg-12">
-        <section class="product_description_area" style="margin-top: -10px; margin-bottom: 100px">
-          <div class="container">
-            <ul class="nav nav-tabs" id="myTab" role="tablist">
-              <li class="nav-item">
-                <a class="nav-link active show" id="qrcode-tab" data-toggle="tab" href="#qrcode" role="tab"
-                   aria-controls="home" aria-selected="true">QrCode</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" id="qrcode2-tab" data-toggle="tab" href="#qrcode2" role="tab"
-                   aria-controls="profile" aria-selected="false">QrCode dengan Logo</a>
-              </li>
-            </ul>
-            <div class="tab-content" id="myTabContent">
-              <div class="tab-pane fade show active" id="qrcode" role="tabpanel" aria-labelledby="qrcode-tab"
-                   style="color: black">
-                <div class="table-responsive">
-                  <table class="table">
-                    <tbody>
-                    <tr>
-                      <td class="text-center">
-                        <h5 style="color: black"> Nomer Binaan : {{$binaan->no_umkm}} </h5>
-                        <a href="data:image/png;base64, {!! base64_encode($downloadQrCode) !!} " download>
-                          <img src="data:image/png;base64, {!! base64_encode($qrcode) !!} ">
-                        </a>
-                        <p>*Pasang kode ini di web anda untuk menampilkan QRcode anda di web</p>
-                        <p><b>*Berlaku hingga Agustus 2022</b></p>
-                        <button style="margin-bottom : 10px" type="button" onclick="Copy()" class="btn btn-primary">
-                          Copy
-                        </button>
-                        <textarea readonly rows="15" id="myQrCode" name="description"
-                                  placeholder="informasi mengenai deskripsi yang menarik" id="description"
-                                  class="form-control"><a href="http://halal.its.ac.id/binaan/{{$binaan->no_umkm}}"><img src="data:image/png;base64, {!! base64_encode($qrcode) !!} "></a></textarea>
-                      </td>
-                    </tr>
-
-
-                    </tbody>
-                  </table>
-                </div>
-              </div>
-              <div class="tab-pane fade" id="qrcode2" role="tabpanel" aria-labelledby="qrcode-tab">
-                <div class="table-responsive">
-                  <table class="table">
-                    <tbody>
-
-                    <tr>
-                      <td class="text-center">
-                        <h5 style="color: black"> Nomer Binaan : {{$binaan->no_umkm}} </h5>
-                        <a href="data:image/png;base64, {!! base64_encode($downloadQrcodeWithLogo) !!} " download>
-                          <img src="data:image/png;base64, {!! base64_encode($qrcodeWithLogo) !!} ">
-                        </a>
-                        <p>*Pasang kode ini di web anda untuk menampilkan QRcode anda di web</p>
-                        <p><b>*Berlaku hingga Agustus 2022</b></p>
-                        <button style="margin-bottom : 10px" type="button" onclick="CopyLogo()" class="btn btn-primary">
-                          Copy
-                        </button>
-                        <textarea readonly rows="15" id="myQrCodeLogo" name="description"
-                                  placeholder="informasi mengenai deskripsi yang menarik" id="description"
-                                  class="form-control"><a href="http://halal.its.ac.id/binaan/{{$binaan->no_umkm}}"><img src="data:image/png;base64, {!! base64_encode($qrcodeWithLogo) !!} "></a></textarea>
-                      </td>
-                    </tr>
-
-                    </tbody>
-                  </table>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-      </div>
-
-    </div>
-  </div>
-  </div>
-
+  @component('halal.home.components.qrcode', [
+    'idBinaan' => $binaan->no_umkm,
+    'plainQRCode' => $plainQRCode,
+    'logoQRCode' => $logoQRCode,
+    'urlBinaan' => route('detail.binaan', ['no_binaan' => $binaan->no_umkm])
+  ])
+  @endcomponent
 @endsection
 
 @if(!empty($jsonld ?? ''))
-  @section('jsonld')
-    <script type="application/ld+json">
-      @json($jsonld)
-    </script>
-  @endsection
+@section('jsonld')
+  <script type="application/ld+json">
+    @json($jsonld)
+  </script>
+@endsection
 @endif
 
