2991df7be631def561aeb32d8956559235faf45d
nadhifikbarw <nadhifikbarw@gmail.com>
2021-10-11 05:23:37 +0700

    ADD: correct handler for provinces

--- database/seeds/CreateCitiesSeeder.php
+++ database/seeds/CreateCitiesSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\City;
+use App\Province;
 use League\Csv\Reader;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -18,10 +19,14 @@ class CreateCitiesSeeder extends Seeder
         $dataUser = Reader::createFromPath(database_path('seeds/csv/cities.csv'), 'r');
         $dataUser->setDelimiter(',');
         $dataUser->setHeaderOffset(0);
+
+
         foreach ($dataUser as $data) {
+            $province = Province::firstOrCreate(['id' => $data['province_id']], ['name' => $data['province_name']]);
+
             City::create([
-                'province_id' => $this->nullChecker($data['province_id']),
-                'name' => $this->nullChecker($data['name']),
+                'province_id' => $province->id,
+                'name' => $data['name'],
             ])->id;
         }
     }
