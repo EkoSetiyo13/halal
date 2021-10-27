8ed0fca4e5c1165afab7d3eb0b33b837de44128a
EkoSetiyo13 <eko.setiyobp@gmail.com>
2021-10-27 20:16:14 +0700

    eko: perbaikan bug route

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ Route::get('/kader/json', 'Halal\HalalController@kaderJson')->name('kader.json')
 Route::get('/kader/{no_binaan}', 'Halal\HalalController@detailKader')->name('detail.kader');
 Route::get('/kader/{no_binaan}/json', 'Halal\HalalController@detailKaderJson')->name('detail.kader.json');
 Route::get('/binaan', 'Halal\HalalController@binaan')->name('binaan');
-Route::get('/binaan/all', 'Halal\HalalController@binaan')->name('binaan');
+// Route::get('/binaan/all', 'Halal\HalalController@binaan')->name('binaan');
 Route::get('/binaan/json', 'Halal\HalalController@binaanJson')->name('binaan.json');
 Route::get('/binaan/{no_binaan}', 'Halal\HalalController@detailBinaan')->name('detail.binaan');
 Route::get('/binaan/{no_binaan}/json', 'Halal\HalalController@detailBinaanJson')->name('detail.binaan.json');
